c3b558c66ebcbeedb2a157729c31a36162a56929
Sc1pex <scipex.dev@gmail.com>
2025-11-14 13:35:33 +0200

    v2 build.zig

--- build.zig
+++ build.zig
@@ -1,9 +1,26 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const asio = b.addLibrary(.{
+        .name = "asio",
+        .root_module = b.createModule(.{
+            .target = b.graph.host,
+        }),
+    });
+    asio.root_module.addIncludePath(b.path("asio/include"));
+    asio.linkLibCpp();
+    asio.root_module.addCMacro("ASIO_SEPARATE_COMPILATION", "1");
+    asio.root_module.addCSourceFile(.{
+        .file = b.path("asio/src/asio.cpp"),
+        .flags = &.{
+            "-std=c++23",
+        },
+    });
+
+    b.installArtifact(asio);
     b.installDirectory(.{
         .source_dir = b.path("asio/include"),
-        .install_dir = .header,
-        .install_subdir = "",
+        .install_dir = .prefix,
+        .install_subdir = "include",
     });
 }
