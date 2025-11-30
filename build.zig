8d1c905ac3322fe035e58f74340882c2aa97e61f
Sc1pex <scipex.dev@gmail.com>
2025-11-30 15:38:31 +0200

    fetch and build asio

--- build.zig
+++ build.zig
@@ -1,5 +1,32 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    _ = b; // stub
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const upstream = b.dependency("asio", .{});
+
+    const asio = b.addLibrary(.{
+        .name = "asio",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .link_libcpp = true,
+        }),
+    });
+    asio.root_module.addIncludePath(upstream.path("include"));
+    asio.root_module.addCMacro("ASIO_SEPARATE_COMPILATION", "1");
+    asio.root_module.addCSourceFile(.{
+        .file = upstream.path("src/asio.cpp"),
+        .flags = &.{
+            "-std=c++23",
+        },
+    });
+    asio.installHeadersDirectory(
+        upstream.path("include"),
+        "",
+        .{ .include_extensions = &.{".hpp"} },
+    );
+
+    b.installArtifact(asio);
 }
